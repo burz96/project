952e9c18bf6345c4087fa61994f7a491ebdbf910
pengowen123 <pengowen816@gmail.com>
2016-03-06 16:41:26 -0700

    Finish electrical system framework

--- avionics.lua
+++ avionics.lua
@@ -1,5 +1,15 @@
 size = { 2048, 2048 }
 
+function createDataRef(drefNamec, drefLocalc)
+    defineProperty(drefNamec, createGlobalPropertyf(drefLocalc))
+    return drefLocalc
+end
+
+function createDataRefi(drefNameI, drefLocalI)
+    defineProperty(drefNameI, createGlobalPropertyi(drefLocalI))
+    return drefLocalI
+end
+
 function geti(name)
 	return get(globalPropertyi(name))
 end
@@ -16,10 +26,83 @@ function setf(name, val)
 	set(globalPropertyf(name), val)
 end
 
-foo = "sim/cockpit/radios/nav1_freq_hz"
+-- Electrical systems
 
-seti(foo, 1)
+function newNode(dref)
+	node = {}
+	node.dref = dref
+	node.power = 0
+	node.nodes = {}
+	return node
+end
+
+function addNodes(node, leaves)
+	new = node
+
+	for i, x in pairs(leaves) do
+		new.nodes[i] = x
+	end
+
+	return new
+end
+
+function power_(node, power, set)
+	if set then
+		node.power = power
+	else
+		node.power = node.power + power
+	end
+
+	for i, n in pairs(node.nodes) do
+		power_(n, power, set)
+	end
+end
+
+function update_(node)
+	if node.power > 0 then
+		seti(node.dref, 1)
+	else
+		seti(node.dref, 0)
+	end
+
+	for i, n in pairs(node.nodes) do
+		update_(n)
+	end
+end
+
+function updateNodes(nodes, powers)
+	for i, n in pairs(nodes) do
+		power_(n, 0, true)
+	end
+
+	for i, n in pairs(nodes) do
+		power_(n, powers[i], false)
+	end
+
+	for i, n in pairs(nodes) do
+		update_(n)
+	end
+end
+
+dr1 = createDataRefi("dr1", "shaz/dr1")
+dr2 = createDataRefi("dr2", "shaz/dr2")
+dr3 = createDataRefi("dr3", "shaz/dr3")
+
+foo = newNode(dr1)
+bar = newNode(dr2)
+qux = newNode(dr3)
+
+bar = addNodes(bar, { foo })
+qux = addNodes(qux, { foo })
 
 function update()
-	seti(foo, geti(foo) + 1)
+	updateNodes(
+	{
+		bar,
+		qux
+	},
+	{
+		0,
+		1
+	})
 end
