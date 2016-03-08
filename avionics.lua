cd01dbd234a696f3e73eb84f470529c04b65c05b
pengowen123 <pengowen816@gmail.com>
2016-03-07 19:07:26 -0700

    Add small example

--- avionics.lua
+++ avionics.lua
@@ -26,7 +26,8 @@ function setf(name, val)
 	set(globalPropertyf(name), val)
 end
 
--- Electrical systems
+
+-- Electrical
 
 function newNode(dref)
 	node = {}
@@ -84,26 +85,82 @@ function updateNodes(nodes, powers)
 	end
 end
 
-test = createDataRefi("test", "shaz/test")
-dr1 = createDataRefi("dr1", "shaz/dr1")
-dr2 = createDataRefi("dr2", "shaz/dr2")
-dr3 = createDataRefi("dr3", "shaz/dr3")
 
-foo = newNode(dr1)
-bar = newNode(dr2)
-qux = newNode(dr3)
+-- Hydraulics
 
-bar = addNodes(bar, { foo })
-qux = addNodes(qux, { foo })
+drfoo = createDataRefi("foo", "dc10/foo")
+drthing = createDataRefi("dr1", "dc10/dr1")
+drgen1 = createDataRefi("dr2", "dc10/dr2")
+drgen2 = createDataRefi("dr3", "dc10/dr3")
+drapu = createDataRefi("dr4", "dc10/dr4")
+drapuswitch = createDataRefi("dr5", "dc10/test")
+
+thing = newNode(drthing)
+gen1 = newNode(drgen1)
+gen2 = newNode(drgen2)
+apu = newNode(drapu)
+
+gen1 = addNodes(gen1, { thing })
+gen2 = addNodes(gen2, { thing })
+apu = addNodes(apu, { thing })
+
+foo = 0
+
+apu_timer = 0
+
+seti(drapuswitch, 1)
+
+gen1_power = 0
+gen2_power = 0
+apu_power = 0
 
 function update()
+	print(geti(drapuswitch))
+
+	if apu_timer > 88 then
+		gen1_power = 1
+		gen2_power = 1
+	end
+
+	if gen1_power == 0 and gen2_power == 0 then
+		apu_power = geti(drapuswitch)
+	else
+		apu_power = 0
+	end
+
+	if apu_power > 0 then
+		if apu_timer < 90 then
+			apu_timer = apu_timer + 1
+		end
+	else
+		apu_timer = apu_timer - 1
+	end
+
+	print("timer: ", apu_timer)
+
 	updateNodes(
 	{
-		bar,
-		qux
+		gen1,
+		gen2,
+		apu
 	},
 	{
-		0,
-		0
+		gen1_power,
+		gen2_power,
+		apu_power
 	})
+
+	print("thing power: ", thing.power, " apu: ", apu.power, " generators: ", geti(gen1.dref), geti(gen2.dref))
+
+	if thing.power == 0 then
+		if foo > 0 then
+			foo = foo - 1
+		end
+	else
+		if foo < 100 then
+			foo = foo + 1
+		end
+	end
+
+	seti(drfoo, foo)
 end
